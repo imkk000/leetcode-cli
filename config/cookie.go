0a51fa91f959932b647975df00c6a70d671465ee
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2022-08-18 02:10:20 +0700

    Add read cookie config

---
+++ config/cookie.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"net/url"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+func ReadCookies() url.Values {
+	cookies := url.Values{}
+
+	vp := viper.New()
+	vp.AddConfigPath(".data/config")
+	vp.SetConfigType("json")
+	vp.SetConfigName("cookies")
+	if err := vp.ReadInConfig(); err != nil {
+		return cookies
+	}
+
+	for k, v := range vp.GetStringMapString("Request Cookies") {
+		if k == "new_problemlist_page" || k == "leetcode_session" {
+			k = strings.ToUpper(k)
+		}
+		cookies.Set(k, v)
+	}
+	return cookies
+}
