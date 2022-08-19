61e02fc20c7eda27d7a6e65ba99bd2d7516613fd
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2022-08-20 06:31:23 +0700

    Add new commands

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ func main() {
 		cmd.VersionCmd,
 		cmd.RunCmd,
 		cmd.InfoCmd,
+		cmd.GenerateFileCmd,
+		cmd.TestNetCmd,
+		cmd.TestCmd,
 	)
 	if err := cd.Execute(); err != nil {
 		fmt.Println("run:", err)
