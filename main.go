7dd483c5a30633fb17b0c66f55f851dfc9e5964e
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2022-08-21 05:10:29 +0700

    Split command

--- main.go
+++ main.go
@@ -3,20 +3,31 @@ package main
 import (
 	"fmt"
 	"leetcode-tool/cmd"
+	"leetcode-tool/config"
 
 	"github.com/spf13/cobra"
 )
 
 func main() {
 	cd := cobra.Command{}
+
+	// implementing command
+	if config.IsDebugMode() {
+		cd.AddCommand(
+			cmd.GenerateFileCmd,
+			cmd.TestCmd,
+		)
+	}
+
+	// publish command
 	cd.AddCommand(
 		cmd.VersionCmd,
 		cmd.RunCmd,
 		cmd.InfoCmd,
-		cmd.GenerateFileCmd,
+		cmd.ParseFileCmd,
 		cmd.TestNetCmd,
-		cmd.TestCmd,
 	)
+
 	if err := cd.Execute(); err != nil {
 		fmt.Println("run:", err)
 	}
