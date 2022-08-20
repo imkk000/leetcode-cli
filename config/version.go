31e14459602471b031e2a6e582a5450d8d5c652e
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2022-08-21 05:09:57 +0700

    Add debug mode in config

--- config/version.go
+++ config/version.go
@@ -2,4 +2,16 @@ package config
 
 var (
 	Version = "1.0.0"
+	Debug   = "true"
 )
+
+func IsDebugMode() bool {
+	return Debug == "true"
+}
+
+func GetMode() string {
+	if IsDebugMode() {
+		return "debug"
+	}
+	return "release"
+}
