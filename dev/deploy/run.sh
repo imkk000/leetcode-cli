3b6ea69b8cc6c9cdfa010ea8c92c7427c1d629a2
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2022-08-21 05:09:02 +0700

    Update deployment script

--- dev/deploy/run.sh
+++ dev/deploy/run.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-sudo id > /dev/null
+sudo id >/dev/null
 
 FILE='main.tmp'
 LOCAL_FILE='/usr/local/bin/lcc'
+GO_MODULE_NAME=$(grep '^module' go.mod | awk '{printf "%s", $2}')
+BUILD_FLAGS="-w -s -X $GO_MODULE_NAME/config.Debug=false"
 
-go build -ldflags="-w -s" -o $FILE .
+echo go module name is $GO_MODULE_NAME
+echo build binary from $FILE to $LOCAL_FILE
+echo build with flags $BUILD_FLAGS
+go build -ldflags="$BUILD_FLAGS" -o $FILE .
 
 if [ ! -f "$FILE" ]; then
-    echo "$FILE does not exist."
+  echo $FILE does not exist
 fi
 
+echo move file to $LOCAL_FILE
 sudo mv $FILE $LOCAL_FILE
